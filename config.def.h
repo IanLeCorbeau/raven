5933a430a1236bf64d6c1478324d4996774ed315
Ian LeCorbeau <59744736+I-LeCorbeau@users.noreply.github.com>
2021-02-19 19:49:05 -0500

    Fix layout names + bstack key

--- config.def.h
+++ config.def.h
@@ -49,9 +49,9 @@ static const int resizehints = 0;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "TT",	      bottomstack }, /* Master at top, stack at bottom */
+	{ "TT",	      bstack }, /* Master at top, stack at bottom */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
-	{ "[]",      monocle },
+	{ "[]",       stackmode },
 };
 
 /* key definitions */
@@ -77,7 +77,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,			xk_b,	   setlayout,	   {.v = &layouts[1]} },
+	{ MODKEY,			XK_b,	   setlayout,	   {.v = &layouts[1]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
